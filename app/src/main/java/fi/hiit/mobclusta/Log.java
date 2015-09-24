d55e10ffa8ed24561ec97a010f7b468a721d4a8e
Jarno Leppänen <jarno.leppanen@iki.fi>
2015-09-24 20:54:43 +0300

    Initial commit

---
+++ app/src/main/java/fi/hiit/mobclusta/Log.java
@@ -0,0 +1,17 @@
+package fi.hiit.mobclusta;
+
+public class Log {
+    public static final String TAG = "mob-clusta";
+    public static void d(String format, Object... args)
+    {
+        android.util.Log.d(TAG, String.format(format, args));
+    }
+    public static void w(String format, Object... args)
+    {
+        android.util.Log.w(TAG, String.format(format, args));
+    }
+    public static void e(String format, Object... args)
+    {
+        android.util.Log.e(TAG, String.format(format, args));
+    }
+}
---
+++ app/src/main/java/fi/hiit/mobclusta/MainActivity.java
@@ -0,0 +1,22 @@
+package fi.hiit.mobclusta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            SlidingTabsBasicFragment fragment = new SlidingTabsBasicFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
+    }
+
+}
