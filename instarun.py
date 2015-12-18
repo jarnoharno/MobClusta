4fe6173e43668c90813fc50facd36aae83bed021
Jarno Leppänen <jarno.leppanen@iki.fi>
2015-12-18 09:45:44 +0200

    Cross-platform instarun.py

--- instarun.py
+++ instarun.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
 
 out = subprocess.check_output(['adb','devices'],universal_newlines=True).strip()
 dev = [s.split()[0] for s in out.split('\n')[1:]]
 
-subprocess.call(['./gradlew','--daemon','installDebug'])
+gradle = './gradlew'
+if os.name == 'nt':
+	gradle = 'gradlew.bat'
+
+subprocess.call([gradle,'--daemon','uninstallAll'])
+subprocess.call([gradle,'--daemon','installDebug'])
 
 for d in dev:
     subprocess.call(['adb','-s',d,'shell','am','start','-n',
